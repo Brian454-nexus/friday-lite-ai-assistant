a55a9b43f13c318dabd37398304b2a244f6f4283
Brian454-nexus <brian.terer@student.moringaschool.com>
2025-03-26 16:18:30 +0300

    enabled response

--- frontend/script.js
+++ frontend/script.js
@@ -2,4 +2,11 @@ let chatHistory = [];
 
 async function sendMessage(message) {
     chatHistory.push({ sender: 'You', text: message });
+    const response = await fetch('http://localhost:3000/send-message', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message })
+    });
 
