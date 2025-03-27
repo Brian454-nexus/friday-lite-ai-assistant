6638944205fb892dbb5346611eeff93ed4654ffd
Brian454-nexus <brian.terer@student.moringaschool.com>
2025-03-28 01:38:24 +0300

    fixed public

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const uuid = require("uuid");
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "/")));
+app.use(express.static(__dirname)); // Serve static files from the root
 
 // Routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "index.html")); // Serve index.html from the root
 });
 
 // Simulated Dialogflow API endpoint
